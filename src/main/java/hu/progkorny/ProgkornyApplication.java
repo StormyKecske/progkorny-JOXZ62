b3f858943324c0e7a2913f1df84ed51b86a5a8c1
Máté János Milán <matemilan2003@gmail.com>
2026-06-05 08:33:36 +0200

    InitData add to test

--- src/main/java/hu/progkorny/ProgkornyApplication.java
+++ src/main/java/hu/progkorny/ProgkornyApplication.java
@@ -1,11 +1,25 @@
 package hu.progkorny;
 
+import hu.progkorny.model.Student;
+import hu.progkorny.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProgkornyApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ProgkornyApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initData(StudentRepository repo) {
+        return args -> {
+            repo.save(new Student("Teszt Elek"));
+            repo.save(new Student("Kiss Béla"));
+            repo.save(new Student("Nagy Anna"));
+        };
     }
 }
